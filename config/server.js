b5b928ca7b78d70e17389081336d54a63fe3af79
Alexandre <alexou73@gmail.com>
2020-09-18 10:03:38 +0200

    add s3

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 8484),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "c704fe5a37a32b70984f4ceddb882bda"),
