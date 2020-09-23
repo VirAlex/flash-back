fd2290cf4e1aca9e971f7f06a3e5a2ac2231ba79
Alexandre <alexou73@gmail.com>
2020-09-23 08:10:34 +0200

    add stripe

--- api/commande/controllers/commande.js
+++ api/commande/controllers/commande.js
@@ -1,8 +1,42 @@
 'use strict';
+const stripe = require('stripe')('sk_test_51H5slxCIQMePPibU8Ld76H0Tteu7V7KOkJQk0TZGNijyXCbg7Hiqfah1cCa4nDV6hvT3ABtwE2QWZ2ZhiI8uiGFr00CIH0w8vo');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  create: async ctx => {
+    console.log(ctx.request.body)
+    const {
+      firstName,
+      amount,
+      lastName,
+      email,
+      token
+    } = ctx.request.body;
 
-module.exports = {};
+    // Charge the customer
+    try {
+      await stripe.charges.create({
+        // Transform cents to dollars.
+        amount: amount,
+        currency: 'eur',
+        description: `Test`,
+        source: token,
+      });
+
+      // Register the order in the database
+      try {
+        const commande = await strapi.services.commandes.create({
+          firstName,
+          amount,
+          lastName,
+          email
+        });
+
+        return commande;
+      } catch (err) {
+        // Silent
+      }
+    } catch (err) {
+      // Silent
+    }
+  },
+};
